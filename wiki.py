5f5ad15c857b144825b754b7a7c9ab9c8d39eef2
Matt Larson <matthewwendelllarson@yahoo.com>
2019-07-24 21:24:50 -0500

    Adding read from CSV capability

--- wiki.py
+++ wiki.py
@@ -1,7 +1,21 @@
 import wikipediaapi
 from googlesearch import search 
+import csv
 
 #load name, birth year from file
+
+names = []
+years = []
+
+with open('standardinput.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count != 0:
+        	names.append(row[0])
+        	years.append(row[1])
+        line_count += 1
+
 #google search name + birth year + wiki 
 
 wiki_wiki = wikipediaapi.Wikipedia(
@@ -11,4 +25,7 @@ wiki_wiki = wikipediaapi.Wikipedia(
 page_py = wiki_wiki.page('Philby')
 print(page_py.text)
 
+#check if it is the birth year
+
 #write to file as CSV
+#add description to CSV?
